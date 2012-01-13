68cc2b5b0906afe1d558bc6375f1078348a08c15
Bernd-Helge Schaefer <schaefer@robotmakers.de>
2012-01-13 09:00:38 +0100

    VideoRenderer: added support for image scaling

--- widgets/VideoRenderer.java
+++ widgets/VideoRenderer.java
@@ -23,8 +23,15 @@ package org.finroc.tools.gui.widgets;
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.event.MouseEvent;
+import java.awt.BorderLayout;
+import java.awt.Cursor;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.Graphics;
 
 import javax.imageio.ImageIO;
 import javax.naming.OperationNotSupportedException;
@@ -36,6 +43,9 @@ import org.finroc.tools.gui.WidgetPort;
 import org.finroc.tools.gui.WidgetUI;
 import org.finroc.tools.gui.commons.fastdraw.BufferedImageRGB;
 import org.finroc.tools.gui.util.gui.FileDialog;
+import org.finroc.tools.gui.util.gui.MAction;
+import org.finroc.tools.gui.util.gui.MActionEvent;
+import org.finroc.tools.gui.util.gui.MToolBar;
 import org.finroc.plugins.data_types.Blittable;
 import org.finroc.plugins.data_types.HasBlittable;
 
@@ -46,6 +56,8 @@ import org.finroc.core.port.PortListener;
 
 public class VideoRenderer extends Widget {
 
+  enum ScaleMode { scaleFast, scaleSmooth, scaleAreaAverage };
+
     /** UID */
     private static final long serialVersionUID = -8452479527434504700L;
 
@@ -53,6 +65,17 @@ public class VideoRenderer extends Widget {
 
     /** Image index in image source - in case we receive lists of blittables */
     public int imageIndexInSource;
+
+    /** Shows or hides the tool bar*/
+    private boolean hideToolbar = true;
+
+    /** Activates image scaling */
+    private boolean scaleImage = false;
+
+    /** Indicates whether to keep image aspect ration or not */
+    private boolean keepAspectRatio = true;
+
+  private ScaleMode scaleMode = ScaleMode.scaleFast;
 
     @Override
     protected WidgetUI createWidgetUI() {
@@ -64,17 +87,42 @@ public class VideoRenderer extends Widget {
         return suggestion.derive(HasBlittable.TYPE);
     }
 
-    class VideoWindowUI extends WidgetUI implements PortListener<HasBlittable>, MouseInputListener {
+  enum Action { SaveSingleImage }
+  //  enum Mode { Normal, Move, Zoom, Rotate}
+
+
+  class VideoWindowUI extends WidgetUI implements PortListener<HasBlittable>, MouseInputListener, ActionListener {
+
+        MToolBar toolbar;
 
         /** Dimension of last blitted image */
         private int lastWidth, lastHeight;
 
         public VideoWindowUI() {
             super(RenderMode.Cached);
+            this.setLayout(new BorderLayout());
             videoInput.addChangeListener(this);
             addMouseListener(this);
             addMouseMotionListener(this);
+
+            toolbar = new MToolBar("GeometryWidget Control", MToolBar.VERTICAL);
+	    //            toolbar.addToggleButton(new MAction(Mode.Normal, "arrow.png", "Point Mode", this, Cursor.HAND_CURSOR));
+	    toolbar.add(new MAction(Action.SaveSingleImage, "document-save.png", "Save Image", this));
+
+            add(toolbar, BorderLayout.WEST);
         }
+
+        public void actionPerformed(ActionEvent ae) {
+	  Enum e = ((MActionEvent)ae).getEnumID();
+
+	  if (e == Action.SaveSingleImage) {
+	    System.out.println ("save image.\n");
+	  }
+	}
+
+	public void widgetPropertiesChanged() {
+	  toolbar.setVisible(!hideToolbar);
+	}
 
         /** UID */
         private static final long serialVersionUID = -922703839059777637L;
@@ -93,7 +141,15 @@ public class VideoRenderer extends Widget {
                 lastWidth = bl.getWidth();
                 lastHeight = bl.getHeight();
             }
-            bl.blitTo(cache, new Rectangle(renderSize));
+
+	    if (scaleImage) {
+	      BufferedImageRGB input_image = new BufferedImageRGB(bl.getWidth(), bl.getHeight());
+	      bl.blitTo(input_image);   
+
+	      bl = this.getScaledInstance (input_image, renderSize, scaleMode, keepAspectRatio);
+	    }
+
+	    bl.blitTo (cache, new Rectangle(renderSize));
             releaseAllLocks();
         }
 
@@ -141,5 +197,53 @@ public class VideoRenderer extends Widget {
             }
         }
 
+	private BufferedImageRGB getScaledInstance (BufferedImageRGB input_image, Dimension renderSize, ScaleMode scaleMode, boolean keepAspectRatio) {
+	  int scale_mode = Image.SCALE_FAST;
+  
+	  if (scaleMode == ScaleMode.scaleFast) {
+	    scale_mode = Image.SCALE_FAST;
+	  }
+	  if (scaleMode == ScaleMode.scaleSmooth) {
+	    scale_mode = Image.SCALE_SMOOTH;
+	  }
+	  if (scaleMode == ScaleMode.scaleAreaAverage) {
+	    scale_mode = Image.SCALE_AREA_AVERAGING;
+	  }
+	  Image scaled_image = null;
+	  if (keepAspectRatio) {
+
+	    double x_aspect = (double) input_image.getWidth() / (double) renderSize.width; 
+	    double y_aspect = (double) input_image.getHeight() / (double) renderSize.height;
+
+	    if (x_aspect > y_aspect) {
+	      scaled_image = input_image.getBufferedImage().getScaledInstance(renderSize.width,
+									      -1, 
+									      scale_mode);
+
+	    }
+	    else {
+	      scaled_image = input_image.getBufferedImage().getScaledInstance(-1,
+									      renderSize.height, 
+									      scale_mode);
+
+	    }
+	  }
+	  else {
+	    scaled_image = input_image.getBufferedImage().getScaledInstance(renderSize.width,
+									    renderSize.height, 
+									    scale_mode);
+	  }
+	      
+	  BufferedImageRGB output_image = new BufferedImageRGB(renderSize.width,
+							       renderSize.height);
+	      
+	  Graphics g = output_image.getBufferedImage().createGraphics();
+	  g.drawImage(scaled_image, 0, 0, null);
+	  g.dispose();
+
+	  return output_image;
+	}
+
     }
 }
+
